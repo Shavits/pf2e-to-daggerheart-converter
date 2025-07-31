74bce32389ae87a8c67a47bf9a2e2604ffd40dc2
shavits <s.shavit4@gmail.com>
2025-07-31 15:38:16 +0300

    added pf data

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+main.py - Entry point for the PyQt desktop application.
+"""
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    # TODO: Initialize and start the PyQt application here
+    main()
